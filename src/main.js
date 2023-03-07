023f11dafcee2c426626b7786297fa20c11ffe71
ethan-choshsh <ethan.s.cho@polarissharetech.com>
2023-03-07 18:23:28 +0900

    Init

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import OneSignalVuePlugin from '@onesignal/onesignal-vue3';
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(OneSignalVuePlugin, { appId: 'e901ae0c-8ee5-4729-bec6-cfd3c12cb101' })
+  .mount('#app');
